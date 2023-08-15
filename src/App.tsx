ce99def5de159c306bfcc25b50794ee15c75daf6
SAMUEL DA SILVA ORTEGA <07060043101@ifms.edu.br>
2023-08-15 07:34:15 -0400

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Header from './componentes/header/Header'
+import './App.css'
+export default function App() {
+  return (
+    <Header/>
+  )
+}
+
+
---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,68 @@
+import'./Header.css'
+export default function Header(){
+  return(
+    <body>
+        
+    <header>
+      <div>
+     <img src = "./public/logo.com.png" alt ="logo"></img>
+      <p>Estreia mundial</p>
+      </div>
+      <nav className = "navbar">
+        <ul>
+          <li>
+          <a href = "#">Home</a>
+          </li>
+          <li>
+          <a href = "#">Noticias</a>
+          </li>
+          <li>
+          <a href = "#">Sobre</a>
+          </li>
+        </ul>
+      </nav>
+     
+    </header>
+    <footer className="footer">
+    <div>
+    <img src="./public/logo.com.png" alt="Logo da Empresa" className="footer-logo" />
+    <p>&copy; 2023 Nome da Sua Empresa. Todos os direitos reservados.</p>
+  </div>
+        
+      
+      <div className="navbar">
+        <ul>
+          <li>
+            <a href="#">Termos de Uso</a>
+          </li>
+          <li>
+            <a href="#">Política de Privacidade</a>
+          </li>
+          <li>
+            <a href="#">Contato</a>
+          </li>
+        </ul>
+        <ul className="footer-links">
+          <li>
+            <a href="#">Produtos</a>
+          </li>
+          <li>
+            <a href="#">Empresas</a>
+          </li>
+          <li>
+            <a href="#">Ajuda</a>
+          </li>
+          <li>
+            <a href="#">Comunidade</a>
+            </li>
+        </ul>
+      </div>
+    </footer>
+
+    </body>
+  )
+}
+
+  
+
+
