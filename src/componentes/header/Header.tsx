fd83c61d58f44e8a74db7460a2cc1106ad260061
SMU2023 <samuel.ortega@estudante.ifms.edu.br>
2023-09-28 10:01:06 -0400

    aula 28/09

--- src/componentes/header/Header.tsx
+++ src/componentes/header/Header.tsx
@@ -1,26 +1,33 @@
 import './Header.css'
+import {Link} from 'react-router-dom'
 function Header(){
 
     return(
         
         <header>
             <div className = "logo">
-            <img src = "./public/logo.com.png" alt ="logo"></img>
+            {/*<img src = "./public/logo.com.png" alt ="logo"></img>*/}
+            <Link to="/"><img src = "./public/logo.com.png" alt ="logo"></img></Link>
       <p>Estreia mundial</p>
             </div>
             <nav>
                 <ul>
                     <li>
-                        <a href="#">Página inicial</a>
+                        {/*<a href="/">Página inicial</a>*/}
+                        <Link to="/">Pagina inicial</Link>
                     </li>
                     <li>
-                        <a href="#">Sobre</a>
+                        {/*<a href="/sobre">Sobre</a>*/}
+                        <Link to="/sobre">Sobre</Link>
+                        
                     </li>
                     <li>
-                        <a href="#">Notícias</a>
+                       { /*<a href="/noticias">Notícias</a>*/}
+                       <Link to="/noticias">Noticias</Link>
                     </li>
                     <li>
-                        <a href="#">Contato</a>
+                        {/*<a href="/contato">Contato</a>*/}
+                        <Link to="/contato">Contato</Link>
                     </li>
                 </ul>
             </nav>
